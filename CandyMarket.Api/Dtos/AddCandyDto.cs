0982722de041e381ee5e1e03c7ffc26b6eefbff0
Silvestre Luna <slvtrluna@gmail.com>
2019-11-03 18:48:55 -0600

    completed method to add new candy

--- CandyMarket.Api/Dtos/AddCandyDto.cs
+++ CandyMarket.Api/Dtos/AddCandyDto.cs
@@ -3,5 +3,8 @@
     public class AddCandyDto
     {
         public string Name { get; set; }
+        public string Type { get; set; }
+        public string Flavor { get; set; }
+        public string Owner { get; set; }
     }
 }

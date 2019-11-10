ee1999cc223996dfd072b08a39b10f1d03ad400b
Silvestre Luna <slvtrluna@gmail.com>
2019-11-10 15:05:02 -0600

    added functions to update states, console logging obj from axios post

--- candymarket.ui/src/App.js
+++ candymarket.ui/src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import Candies from './Components/Candies/Candies'
 import AddCandy from './Components/AddCandy/AddCandy'
+import getCandies from './helper/data/getCandies';
+
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    allCandies: []
+  }
+
+  getData = () => {
+    getCandies.getAllCandies()
+      .then((resp) => {
+        const allCandies = resp.data
+        this.setState({ allCandies });
+      })
+      .catch();
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
   render() {
     return (
       <div className="App">
         <div>
           <h1>Candy Market</h1>
-          <Candies />
+          <Candies allCandies={this.state.allCandies} />
         </div>
 
         <div>
-          <AddCandy />
+          <AddCandy getData={this.getData} />
         </div>
       </div>
     );
--- candymarket.ui/src/Components/AddCandy/AddCandy.js
+++ candymarket.ui/src/Components/AddCandy/AddCandy.js
@@ -1,26 +1,82 @@
 import React from 'react';
 import addCandy from '../../helper/data/addCandy';
 
+const tempCandyObj = {
+  name: '',
+  type: '',
+  flavor: '',
+  owner: '',
+}
+
+const tempCandyObjEmpty = {
+  name: '',
+  type: '',
+  flavor: '',
+  owner: '',
+}
+
 class AddCandy extends React.Component {
 
-    addNewCandy = () => {
-        const testObj = {
-            name: "anotherone", 
-            type: "testtype",
-            flavor: "testflavor",
-            owner: "testName",
-            isDonate: false
-        }
+  state = {
+    newCandy: tempCandyObj,
+    respFromdbNewCandy: {}
+  }
 
-        addCandy.addCandy(testObj);
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.addNewCandy}>Add a New Candy</button>
-            </div>
-        );
-    }
+  addNewCandyToDB = () => {
+
+    const { newCandy } = this.state;
+
+    addCandy.addCandy(newCandy)
+      .then((response) => {
+        const newCandyAddedToDb = response.config.data;
+        this.setState({ respFromdbNewCandy: JSON.parse(newCandyAddedToDb) });
+        this.props.getData();
+        this.setState({ newCandy: tempCandyObjEmpty });
+        this.displayNewCandyAdded();
+      })
+      .catch(err => console.error(err));
+  }
+
+  displayNewCandyAdded = () => {
+    const candy = this.state.respFromdbNewCandy
+    console.error(candy.name);
+  }
+
+  updateStateValues = (e) => {
+    const inputFieldName = e.target.name
+    const inputFieldValue = e.target.value;
+    tempCandyObj[inputFieldName] = inputFieldValue;
+    this.setState({ newCandy: tempCandyObj });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onChange={this.updateStateValues}>
+          <h5>New Candy: </h5>
+          <div>
+            <label>Name</label>
+            <input type="text" id="candy-name" name="name" />
+          </div>
+          <div>
+            <label>Type</label>
+            <input type="text" id="candy-type" name="type" />
+          </div>
+          <div>
+            <label>Flavor</label>
+            <input type="text" id="candy-flavor" name="flavor" />
+          </div>
+          <div>
+            <label>Owner's Name</label>
+            <input type="text" id="candy-owner" name="owner" />
+          </div>
+        </form>
+        <div>
+          <button className="btn btn-primary" onClick={this.addNewCandyToDB} >Add a New Candy</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AddCandy;
